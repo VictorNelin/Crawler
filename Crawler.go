6d4bccb9ba14e148f7d80215a48c4c79ec5f6d8b
VictorNelin <38560646+VictorNelin@users.noreply.github.com>
2019-01-09 14:22:05 +0300

    Begining

---
+++ Crawler.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	fmt.Println("http transport error is:", err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Println("read error is:", err)
+
+	fmt.Println(string(body))
+}
